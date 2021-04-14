310cf121416608bb7d0cb09f4ae20345a2d445ee
Harshi <72039632+Harshi9843@users.noreply.github.com>
2021-04-14 17:29:59 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {View} from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends Component{
+  render(){
+    return(
+      <WelcomeScreen />
+    )
+  }
+}
